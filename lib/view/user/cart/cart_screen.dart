71eb44086880679cef2547883a0f7c377bc4e601
Eslamadelelkhouly <rodyerak@gmail.com>
2024-12-01 17:45:00 +0200

    add button counter product

--- lib/view/user/cart/cart_screen.dart
+++ lib/view/user/cart/cart_screen.dart
@@ -29,75 +29,159 @@ class _CartScreenState extends State<CartScreen> {
           ),
           height: height,
           width: width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(text: 'Subtotal', style: textTheme.bodyLarge),
-                    TextSpan(
-                      text: '\$ 53313',
-                      style: textTheme.bodyLarge!.copyWith(
-                        fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Subtotal', style: textTheme.bodyLarge),
+                      TextSpan(
+                        text: '\$ 53313',
+                        style: textTheme.bodyLarge!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              CommonFunctions.blankSpace(height * 0.01, 0),
-              SizedBox(
-                height: height * 0.06,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.check_circle,
-                      color: teal,
-                    ),
-                    CommonFunctions.blankSpace(
-                      0,
-                      width * 0.01,
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          RichText(
-                            textAlign: TextAlign.justify,
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                  text:
-                                      'Your Order is eligible for FREE Delivery.',
-                                  style: textTheme.bodySmall!.copyWith(
-                                    color: teal,
+                CommonFunctions.blankSpace(height * 0.01, 0),
+                SizedBox(
+                  height: height * 0.06,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.check_circle,
+                        color: teal,
+                      ),
+                      CommonFunctions.blankSpace(
+                        0,
+                        width * 0.01,
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            RichText(
+                              textAlign: TextAlign.justify,
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text:
+                                        'Your Order is eligible for FREE Delivery.',
+                                    style: textTheme.bodySmall!.copyWith(
+                                      color: teal,
+                                    ),
                                   ),
+                                  TextSpan(
+                                    text:
+                                        'Select this option at this checkout.',
+                                    style: textTheme.bodySmall,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    backgroundColor: amber,
+                    minimumSize: Size(width, height * 0.06),
+                  ),
+                  onPressed: () {},
+                  child: Text('Proceed to Buy', style: textTheme.bodySmall),
+                ),
+                CommonFunctions.blankSpace(height * 0.02, 0),
+                CommonFunctions.divider(),
+                CommonFunctions.blankSpace(height * 0.02, 0),
+                ListView.builder(
+                  itemCount: 2,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: height * 0.2,
+                      width: width,
+                      margin: EdgeInsets.symmetric(
+                        vertical: height * 0.01,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: greyShade1,
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 4,
+                            child: Column(
+                              children: [
+                                Image(
+                                  image: AssetImage(
+                                    'assets/images/todays_deals/todaysDeal1.png',
+                                  ),
+                                  fit: BoxFit.fitWidth,
                                 ),
-                                TextSpan(
-                                  text: 'Select this option at this checkout.',
-                                  style: textTheme.bodySmall,
+                                Container(
+                                  height: height * 0.06,
+                                  width: width,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(color: greyShade3),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        flex: 2,
+                                        child: Container(
+                                          child:
+                                              Icon(Icons.remove, color: black),
+                                          decoration: BoxDecoration(
+                                            border: Border(
+                                              right: BorderSide(
+                                                color: greyShade3,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 3,
+                                        child: Container(
+                                          color: white,
+                                          alignment: Alignment.center,
+                                          child: Text('1'),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 2,
+                                        child: Container(
+                                          child: Icon(Icons.add, color: black),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ],
                             ),
                           ),
+                          Expanded(
+                            flex: 6,
+                            child: Column(),
+                          ),
                         ],
                       ),
-                    ),
-                  ],
+                    );
+                  },
                 ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  backgroundColor: amber,
-                  minimumSize: Size(width, height * 0.06),
-                ),
-                onPressed: () {},
-                child: Text('Proceed to Buy', style: textTheme.bodySmall),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
