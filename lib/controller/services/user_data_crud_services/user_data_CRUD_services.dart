9837885d766761259798471dd041647680de34c6
Eslamadelelkhouly <rodyerak@gmail.com>
2024-12-06 21:33:52 +0200

    add checkuser function

--- lib/controller/services/user_data_crud_services/user_data_CRUD_services.dart
+++ lib/controller/services/user_data_crud_services/user_data_CRUD_services.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:store/constant/common_functions.dart';
 import 'package:store/constant/constants.dart';
 import 'package:store/model/user_model.dart';
 
@@ -16,7 +17,31 @@ class UserDataCRUD {
           .set(usermodel.toMap())
           .whenComplete(() {
         log('Data Added');
+        CommonFunctions.showToast(
+          context: context,
+          message: 'User Added Successful',
+        );
       });
-    } catch (e) {}
+    } catch (e) {
+      log(e.toString());
+      CommonFunctions.showToast(
+        context: context,
+        message: e.toString(),
+      );
+    }
+  }
+
+  static Future checkUser() async {
+    try {
+      await firestore
+          .collection('users')
+          .where('email', isEqualTo: auth.currentUser!.email)
+          .get()
+          .then((value) {
+        value.size > 0 ? true : false;
+      });
+    } catch (e) {
+      log(e.toString());
+    }
   }
 }
